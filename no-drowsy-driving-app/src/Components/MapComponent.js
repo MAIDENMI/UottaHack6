b10e37e529efe8d966eccaafbfe7191cb9e30366
Ryan Frost-Garant <rfros048@uottawa.ca>
2024-03-02 21:46:20 -0500

    markers for charge locations

--- no-drowsy-driving-app/src/Components/MapComponent.js
+++ no-drowsy-driving-app/src/Components/MapComponent.js
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect } from "react";
 import mapboxgl from "mapbox-gl";
-// import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
-// import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css"; // Import directions CSS
+import MapboxDirections from "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions";
+import "@mapbox/mapbox-gl-directions/dist/mapbox-gl-directions.css"; // Import directions CSS
 
 mapboxgl.accessToken =
   "pk.eyJ1IjoicnRpZXJyZSIsImEiOiJjbDg2OWM0b3IwOHExM3ZtcWR5MWlyaXpqIn0.CXyX38b-HvC9pt3kHQc_VA";
@@ -9,6 +9,36 @@ mapboxgl.accessToken =
 export default function MapComponent() {
   const mapContainer = useRef(null);
   const map = useRef(null);
+
+  const charge_loc = useEffect(() => {
+    const fetchData = async() => {
+      const charge_loc = {
+        method: 'GET',
+        url: 'https://ev-charge-finder.p.rapidapi.com/search-by-location',
+        params: {
+          near: 'Ottawa, ON, CAN',
+          limit: '50'
+        },
+        headers: {
+          'X-RapidAPI-Key': 'cd19f935e1msh5743e34a54d2698p16a1d7jsnc88b71a19461',
+          'X-RapidAPI-Host': 'ev-charge-finder.p.rapidapi.com'
+        }
+      };
+  
+      try {
+        const response = await axios.request(charge_loc);
+        console.log(response.data);
+
+
+
+      } catch (error) {
+        console.error(error);
+      }
+    };
+  return () => fetchData();
+
+  },[]);
+  console.log(charge_loc)
 
   useEffect(() => {
     if (map.current) return; 
@@ -20,15 +50,15 @@ export default function MapComponent() {
       zoom: 13,
     });
 
-    // const directions = new MapboxDirections({
-    //   accessToken: mapboxgl.accessToken,
-    //   unit: "metric",
-    //   profile: "mapbox/driving",
-    // });
+    const directions = new MapboxDirections({
+      accessToken: mapboxgl.accessToken,
+      unit: "metric",
+      profile: "mapbox/driving",
+    });
 
-    // map.current.on("load", () => {
-    //   map.current.addControl(directions, "top-right");
-    // });
+    map.current.on("load", () => {
+      map.current.addControl(directions, "top-left");
+    });
 
     return () => map.current && map.current.remove();
   }, []);
